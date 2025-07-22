36b22ffb502ab2e6358e3b95de270ac96d4a9465
byestndr <andrew.k.pham@outlook.com>
2025-07-21 19:42:59 -0500

    Add currently playing item to carousel

--- lib/widgets/songimage.dart
+++ lib/widgets/songimage.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:spotimmich/settings/spotify/spotifyapi.dart';
 
 class SongImage extends StatefulWidget {
-  const SongImage({super.key});
+  final int imageMultiplier;
+  const SongImage({super.key, required this.imageMultiplier});
 
   @override
   State<SongImage> createState() => _SongImageState();
@@ -13,13 +14,13 @@ class SongImage extends StatefulWidget {
 
 class _SongImageState extends State<SongImage> {
   Timer? timer;
-  dynamic songArt = Image.asset('assets/imagePlaceholder.png', width: 300,);
-  static const double imageRadius = 28;
+  dynamic songArt = Image.asset('assets/imagePlaceholder.png', width: 300);
+  late int imageMultiplier;
 
   @override
   void initState() {
     super.initState();
-
+    imageMultiplier = widget.imageMultiplier;
     timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
       RefreshLoop();
     });
@@ -33,11 +34,26 @@ class _SongImageState extends State<SongImage> {
             try {
               final dynamic body = jsonDecode(value);
               final String imageURL = body['item']['album']['images'][0]['url'];
-              songArt = Image.network(imageURL, cacheHeight: (MediaQuery.of(context).devicePixelRatio * 200).toInt(),);
+              songArt = Image.network(
+                imageURL,
+                cacheHeight:
+                    (MediaQuery.of(context).devicePixelRatio * imageMultiplier)
+                        .toInt(),
+              );
             } on FormatException {
-              songArt = Image.asset('assets/imagePlaceholder.png', cacheHeight: (MediaQuery.of(context).devicePixelRatio * 200).toInt(),);
+              songArt = Image.asset(
+                'assets/imagePlaceholder.png',
+                cacheHeight:
+                    (MediaQuery.of(context).devicePixelRatio * imageMultiplier)
+                        .toInt(),
+              );
             } on NoSuchMethodError {
-              songArt = Image.asset('assets/imagePlaceholder.png', cacheHeight: (MediaQuery.of(context).devicePixelRatio * 200).toInt(),);
+              songArt = Image.asset(
+                'assets/imagePlaceholder.png',
+                cacheHeight:
+                    (MediaQuery.of(context).devicePixelRatio * imageMultiplier)
+                        .toInt(),
+              );
             }
           });
     });
@@ -51,29 +67,6 @@ class _SongImageState extends State<SongImage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsetsGeometry.all(16),
-      child: FittedBox(
-        fit: BoxFit.scaleDown,
-        child: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(imageRadius)),
-            color: Colors.transparent,
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                color: Colors.black38,
-                blurRadius: 2,
-                offset: Offset(0, 2),
-              ),
-            ],
-          ),
-
-          child: ClipRRect(
-            borderRadius: BorderRadiusGeometry.circular(imageRadius),
-            child: songArt
-          ),
-        ),
-      ),
-    );
+    return songArt;
   }
 }
