7791610a2464bb714c1e041007675e9d243967e9
byestndr <andrew.k.pham@outlook.com>
2026-05-16 03:24:33 -0500

    [CHORE] Add romanization library

--- lib/widgets/background/immich_background_carousel.dart
+++ lib/widgets/background/immich_background_carousel.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:spotimmich/pages/info_page.dart';
 import 'package:spotimmich/providers/theme/background_getter.dart';
 
 class ImmichCarousel extends ConsumerStatefulWidget {
