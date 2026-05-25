5c8285c615f2cd223f604471ded1efb1f574ea94
byestndr <andrew.k.pham@outlook.com>
2026-05-25 12:32:03 -0500

    [FEATURE] Allow scrolling in lyrics

--- lib/pages/lyrics_page.dart
+++ lib/pages/lyrics_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
@@ -18,11 +20,19 @@ class LyricsPage extends ConsumerStatefulWidget {
 class _LyricsPageState extends ConsumerState<LyricsPage> {
   late ItemScrollController _listController;
   int currentLyricIndex = 0;
+  bool _isScrolling = false;
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
     _listController = ItemScrollController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
   }
 
   @override
@@ -39,6 +49,10 @@ class _LyricsPageState extends ConsumerState<LyricsPage> {
       AsyncValue<int> next,
     ) {
       next.whenData((int value) {
+        if (_isScrolling) {
+          return;
+        }
+
         if (mounted) {
           setState(() {
             currentLyricIndex = value;
@@ -66,25 +80,44 @@ class _LyricsPageState extends ConsumerState<LyricsPage> {
                 behavior: ScrollConfiguration.of(
                   context,
                 ).copyWith(scrollbars: false),
-                child: ScrollablePositionedList.builder(
-                  itemScrollController: _listController,
-                  itemCount: data.length,
-                  padding: EdgeInsets.symmetric(
-                    vertical: MediaQuery.of(context).size.height / 3,
-                  ),
-                  itemBuilder: (BuildContext context, int lineIndex) {
-                    final bool isCurrentLyric = lineIndex == currentLyricIndex;
+                child: NotificationListener(
+                  onNotification: (Notification notification) {
+                    if (notification is ScrollUpdateNotification) {
+                      setState(() {
+                        _isScrolling = true;
+                      });
+                    } else if (notification is ScrollEndNotification) {
+                      _timer?.cancel();
 
-                    return LyricLineWidget(
-                      isCurrentLyric: isCurrentLyric,
-                      lyric: data[lineIndex],
-                    );
+                      _timer = Timer(
+                        const Duration(seconds: 3),
+                        () => setState(() {
+                          _isScrolling = false;
+                        }),
+                      );
+                    }
+                    return false;
                   },
+                  child: ScrollablePositionedList.builder(
+                    itemScrollController: _listController,
+                    itemCount: data.length,
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height / 3,
+                    ),
+                    itemBuilder: (BuildContext context, int lineIndex) {
+                      final bool isCurrentLyric =
+                          lineIndex == currentLyricIndex;
+
+                      return LyricLineWidget(
+                        isCurrentLyric: isCurrentLyric,
+                        lyric: data[lineIndex],
+                      );
+                    },
+                  ),
                 ),
               );
             },
             error: (Object error, StackTrace stackTrace) {
-              print(error.toString());
               return ListView(
                 padding: EdgeInsets.symmetric(
                   vertical: MediaQuery.of(context).size.height / 3,
